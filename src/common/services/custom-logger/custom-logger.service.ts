610d15ee89ecc6253dfa343d64036995b3e9170d
Arlen Manasov <arlen.manasov@icloud.com>
2023-12-07 12:43:19 +0600

    add log to files

--- src/common/services/custom-logger/custom-logger.service.ts
+++ src/common/services/custom-logger/custom-logger.service.ts
@@ -1,4 +1,6 @@
 import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 export enum LogLevel {
   ERROR = 0,
@@ -9,8 +11,21 @@ export enum LogLevel {
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class LoggingService extends ConsoleLogger {
-  private logLevel: LogLevel =
-    (Number(process.env.LOG_LEVEL) as LogLevel) || LogLevel.INFO;
+  private readonly filePath = path.join(__dirname, './application.log');
+  private readonly errorFilePath = path.join(__dirname, './errors.log');
+  private readonly fileSizeLimit = Number(process.env.MAX_LOG_FILE_SIZE);
+
+  private logLevel: LogLevel = process.env.LOG_LEVEL === undefined ? LogLevel.INFO : Number(process.env.LOG_LEVEL)
+
+  async logToFile(message: string, isErrorLog = false) {
+    const datetime = new Date().toISOString();
+    const logMessage = `${datetime}: ${message}\n`;
+
+    const path = isErrorLog ? this.errorFilePath : this.filePath;
+
+    await this.rotateLogFileIfNecessary(path);
+    await fs.writeFile(path, logMessage, { flag: 'a' });
+  }
 
   logRequest(
     method: string,
@@ -19,19 +34,18 @@ export class LoggingService extends ConsoleLogger {
     queryParams: string,
   ) {
     if (this.logLevel >= LogLevel.INFO) {
-      console.log(
-        '\x1b[36m%s\x1b[0m',
-        `[Request] Method: ${method}, Url: ${originalUrl}, Body: ${body}, Query params: ${queryParams}`,
-      );
+      const message = `[Request] Method: ${method}, Url: ${originalUrl}, Body: ${body}, Query params: ${queryParams}`;
+      console.log('\x1b[36m%s\x1b[0m', message);
+
+      this.logToFile(message);
     }
   }
 
   logResponse(statusCode: number, method: string, originalUrl: string) {
     if (this.logLevel >= LogLevel.INFO) {
-      console.log(
-        '\x1b[36m%s\x1b[0m',
-        `[Response] Method: ${method}, Url: ${originalUrl}, Status code: ${statusCode}`,
-      );
+      const message = `[Response] Method: ${method}, Url: ${originalUrl}, Status code: ${statusCode}`;
+      console.log('\x1b[36m%s\x1b[0m', message);
+      this.logToFile(message);
     }
   }
 
@@ -42,16 +56,42 @@ export class LoggingService extends ConsoleLogger {
     message: string | object,
   ) {
     if (this.logLevel >= LogLevel.ERROR) {
-      console.log(
-        '\x1b[31m%s\x1b[0m',
-        `[Error] ${method} ${originalUrl}, Status code: ${statusCode}, Message: ${message}`,
-      );
+      const errorMsg = `[Error] ${method} ${originalUrl}, Status code: ${statusCode}, Message: ${JSON.stringify(
+        message,
+      )}`;
+      console.log('\x1b[31m%s\x1b[0m', errorMsg);
+      this.logToFile(errorMsg, true);
     }
   }
 
   logUnhandledRejection(promise, reason) {
     if (this.logLevel >= LogLevel.ERROR) {
       console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+      this.logToFile(
+        `Unhandled Rejection at: ${promise} reason: ${reason}`,
+        true,
+      );
+    }
+  }
+
+  private async rotateLogFileIfNecessary(path: string) {
+    let fileStat;
+    try {
+      fileStat = await fs.stat(path);
+    } catch (error) {
+      // If it does not exist, exit the function
+      if (error.code === 'ENOENT') {
+        return;
+      }
+      throw error; // If the error is something else, it's unexpected so throw it
+    }
+
+    const { size } = fileStat;
+
+    if (size > this.fileSizeLimit) {
+      const datetime = new Date().toISOString().replace(/:/g, '.');
+      const rotatedFilePath = `${path}.${datetime}`;
+      await fs.rename(path, rotatedFilePath);
     }
   }
 }
